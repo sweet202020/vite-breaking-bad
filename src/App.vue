e47c445e407f5b0e581a7a404bc5a273f835c2e1
sweet202020 <dolcealessio1@gmail.com>
2022-11-14 16:07:04 +0100

    first part of import the image

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <script >
+import axios from 'axios'
 import SiteHeader from './components/SiteHeader.vue';
 export default {
   name: 'App',
   components: {
     SiteHeader,
+  },
+  data() {
+    return {
+      API_URL: 'https://www.breakingbadapi.com/api/characters',
+      characters: null,
+      info: null,
+      error: null
+    }
+  }, methods: {
+    callApi(url) {
+      axios.get(url)
+        .then(resp => {
+          console.log(resp);
+          this.characters = resp.data
+          this.info = resp.data
+          console.log(this.characters);
+
+        })
+    }
+  }, mounted() {
+    this.callApi(this.API_URL)
   }
 }
 </script>
@@ -13,12 +35,26 @@ export default {
     <SiteHeader />
   </header>
   <!-- /#siter_header -->
-  <main id="site_main"></main>
+  <main id="site_main">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5">
+        <div class="card" v-for="character in characters">
+          <div class="col">
+            <img :src="character.img" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
   <!-- /#site_main -->
   <footer id="site_footer"></footer>
   <!-- /#site_footer -->
 </template>
 
 <style scoped>
-
+img {
+  width: 240px;
+  aspect-ratio: 1/1;
+  object-fit: contain;
+}
 </style>
